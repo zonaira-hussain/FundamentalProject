5466952a3bd341350446aba16b57f6d992e3ddfc
zonaira-hussain <zonaira@hotmail.co.uk>
2021-03-04 17:23:40 +0000

    back-end added with testing complete

---
+++ src/test/java/com/qa/ww/SpringFundProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.ww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
